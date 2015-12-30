bc8415e4b3d0e2bde50bd30dd9323792ab6ce02b
LadwaAditya <ladwa.aditya@gmail.com>
2015-12-30 19:22:46 +0530

    Added Adapter

--- app/src/main/java/com/ladwa/aditya/justjava/MainActivity.java
+++ app/src/main/java/com/ladwa/aditya/justjava/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -26,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         //Improve Perfomance by setting fixed content
         mRecyclerView.setHasFixedSize(true);
+
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/ladwa/aditya/justjava/MyAdapter.java
@@ -0,0 +1,42 @@
+package com.ladwa.aditya.justjava;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Aditya on 30-Dec-15.
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private String[] mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView mTextView;
+        public ViewHolder(TextView view) {
+            super(view);
+            mTextView= view;
+        }
+    }
+
+    public MyAdapter(String[] mDataset) {
+        this.mDataset = mDataset;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
